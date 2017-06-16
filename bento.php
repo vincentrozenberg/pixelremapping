49cb5185ef9679d056ca8099881df2c211566514
Vincent Rozenberg <vincentrozenberg@gmail.com>
2017-06-16 04:12:42 +0200

    Create bento.php

---
+++ bento.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>PixelRemapping - Bento</title>
+<link rel="stylesheet" href="/bootstrap/css/bootstrap.vincent.css">
+<link rel="stylesheet" href="/bootstrap/css/custom.css">
+</head>
+<body>   
+<div class="container">
+<div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6"><h2>PixelRemapping | A <strong>Bento</strong> Project</h2></div>
+  <div class="col-md-3"></div></div>
+<div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6">
+
+      
+      <br>
+      <p><i>How does the visual language of an image change after being uploaded to internet?</i></p><br>
+<p>Pixel remapping aims to create an interactive thinking practice of today's art production 
+with up to date technology and image sharing inclinations in terms of the visual language changes.
+</p><br>
+ <p> Bento 2015 / <a href="http://elifoner.co">Elif &#214;ner</a>  &#38; <a href="http://vincentrozenberg.com">Vincent Rozenberg</a></p></div>
+  <div class="col-md-3"></div></div>
+    <div><br></div>
+    <div class="row">
+  <div class="col-md-3"></div>
+  <div class="col-md-6"> 
+      <div class="row">
+          <div class="col-md-6"><p class="text-left"><a href="index.php">Create your own</a>, or <a href="imagedir.php">check</a> the gallery.</p></div>
+          <div class="col-md-6"><p class='text-right'> &copy <a href="/bento.php">Bento</a>  <?php echo date('Y');?></p></div>
+
+      </div></div>
+  <div class="col-md-3">		
+</div></div> </div>
+
+
+</body></html>
